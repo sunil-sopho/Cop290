43ae1c3f0c61989c8ef16c8f0dcb806fb08f5f2c
Sunil Kumar <sunilssaharan@gmail.com>
2018-03-11 02:52:22 +0530

    adding shapes

--- code/cad.cpp
+++ code/cad.cpp
@@ -1,3 +1,10 @@
+/*
+Author - Sunil Kumar
+gitid - sunil-sopho
+Projectname - CAD
+*/
+
+
 #include <SDL2/SDL.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
@@ -8,11 +15,28 @@
 #include "stdlib.h"
 #include "string.h"
 #include  <bits/stdc++.h>
+#include "cube.h"
 using namespace std;
 
 SDL_Window *screen;
 
+// Draw scene for all viewports
 void Drawscene()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+}
+void Drawscene2()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+}
+void Drawscene3()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+}
+void Drawscene4()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -21,7 +45,18 @@ void Drawscene()
 void establishMatrix(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height);
 void initGL(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height);
 
+// differnt matrix for different views 
+void establishMatrix2(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height);
+void initGL2(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height);
 
+void establishMatrix3(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height);
+void initGL3(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height);
+
+void establishMatrix4(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height);
+void initGL4(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height);
+
+
+// main
 int main(int argc, char ** argv)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -38,8 +73,21 @@ int main(int argc, char ** argv)
 	while(!done)\
 	{
 		// done++;
-		initGL(0,0,1024,610);
+		// main viewport for 2D
+		initGL(320,0,340,300);
 		Drawscene();
+
+		//viewport for 3D projection
+		initGL2(320,320,680,300);
+		Drawscene2();
+
+		initGL3(680,0,340,300);
+		Drawscene3();
+
+		// sidebar 
+		initGL4(20,0,280,610);
+		Drawscene4();
+
 		SDL_Event event;
 		while(SDL_PollEvent(&event))
 		{	
@@ -81,3 +129,83 @@ void initGL(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height)
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 	glEnable(GL_PERSPECTIVE_CORRECTION_HINT);
 }	
+
+
+
+
+
+void establishMatrix2(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height)
+{
+	glViewport(lx,ly,width,height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,200.0f);
+}
+
+void initGL2(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height)
+{
+	establishMatrix2(lx,ly,width,height);
+
+	glDisable(GL_SCISSOR_TEST);
+	glClearColor(0.0f,1.0f,1.0f,1.0f);
+	glScissor(lx,ly,width,height);
+	glEnable(GL_SCISSOR_TEST);
+
+	glClearColor(0.00f,0.70f,0.20f,1.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_PERSPECTIVE_CORRECTION_HINT);
+}	
+
+void establishMatrix3(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height)
+{
+	glViewport(lx,ly,width,height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,200.0f);
+}
+
+void initGL3(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height)
+{
+	establishMatrix3(lx,ly,width,height);
+
+	glDisable(GL_SCISSOR_TEST);
+	glClearColor(0.0f,1.0f,1.0f,1.0f);
+	glScissor(lx,ly,width,height);
+	glEnable(GL_SCISSOR_TEST);
+
+	glClearColor(0.80f,0.70f,0.90f,1.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_PERSPECTIVE_CORRECTION_HINT);
+}	
+
+void establishMatrix4(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height)
+{
+	glViewport(lx,ly,width,height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,200.0f);
+}
+
+void initGL4(GLsizei lx, GLsizei ly, GLsizei width, GLsizei height)
+{
+	establishMatrix4(lx,ly,width,height);
+
+	glDisable(GL_SCISSOR_TEST);
+	glClearColor(0.0f,1.0f,1.0f,1.0f);
+	glScissor(lx,ly,width,height);
+	glEnable(GL_SCISSOR_TEST);
+
+	glClearColor(0.50f,0.90f,0.10f,1.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_PERSPECTIVE_CORRECTION_HINT);
+}	
+
---
+++ code/cube.h
@@ -0,0 +1,14 @@
+// #include <GL/gl.h>
+// #include <GL/glu.h>
+// #include <GL/glut.h>
+void cube(){
+	glEnable(GL_COLOR_MATERIAL);
+	glDisable(GL_CULL_FACE);
+
+	glBegin(GL_QUADS);
+
+
+
+	glEnd();
+
+}
