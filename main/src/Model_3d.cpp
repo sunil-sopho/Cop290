64c3e96efb4fe50372cfe25333b36b4581fcadef
root <root@localhost.localdomain>
2018-03-11 11:35:08 +0530

    Makefile working

--- main/src/Model_3d.cpp
+++ main/src/Model_3d.cpp
@@ -1,6 +1,12 @@
 #include <Model_3d.h>
+#include <algorithm>
 // #include <Point.h>
-void Model_3d::addEgde(Point src,Point dest,Model_3d m){
-	m.nodes[src.id].push_back(dest);
-	m.nodes[dest.id].push_back(dest);
-};
+void Model_3d::addEgde(Point src,Point dest){
+	nodes[src.id].push_back(dest);
+	nodes[dest.id].push_back(dest);
+}
+// void Model_3d::removeEgde(Point src,Point dest){
+// 	std::vector<Point>::iterator pos=std::find(nodes[src].begin(),nodes[src].end(),dest);
+// 	if(pos!=nodes[src].end())
+// 		nodes[src].erase(pos);
+// }
---
+++ main/src/main.cpp
@@ -0,0 +1,6 @@
+#include <Model_3d.h>
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
