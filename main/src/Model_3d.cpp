82f5cdad0d480bcf42de263897d8059024487288
anubhav-10 <root@localhost.localdomain>
2018-03-12 22:01:33 +0530

    made changes

--- main/src/Model_3d.cpp
+++ main/src/Model_3d.cpp
@@ -3,10 +3,20 @@
 // #include <Point.h>
 void Model_3d::addEgde(Point src,Point dest){
 	nodes[src.id].push_back(dest);
-	nodes[dest.id].push_back(dest);
+	nodes[dest.id].push_back(src);
+	count=(count<src.id)?src.id?count;
+	count=(count<dest.id)?dest.id?count;
 }
-// void Model_3d::removeEgde(Point src,Point dest){
-// 	std::vector<Point>::iterator pos=std::find(nodes[src].begin(),nodes[src].end(),dest);
-// 	if(pos!=nodes[src].end())
-// 		nodes[src].erase(pos);
-// }
+OrthographicView Model_3d::GetOrthographic(Model_3d m){
+	Projection top;
+	for(int i=0;i<count;i++){
+		vector<Point> v=nodes[i];
+		for(int j=0;j<v.size();j++){
+			Point p;
+			p.x=v[j].x;
+			p.y=0.0;
+			p.z=v[j].z;
+			top.addEgde()
+		}
+	}
+}
